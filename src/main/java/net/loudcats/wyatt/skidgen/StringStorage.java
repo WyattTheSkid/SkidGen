cffaa71a5546fe55fe310ad426719ea1afb52513
Wyatt <wyatth120@gmail.com>
2024-11-04 02:30:29 -0500

    yuh

--- src/main/java/net/loudcats/wyatt/skidgen/StringStorage.java
+++ src/main/java/net/loudcats/wyatt/skidgen/StringStorage.java
@@ -1,7 +1,7 @@
 package net.loudcats.wyatt.skidgen;
 
 public class StringStorage {
-    public static final String verNum =  "1.0";
+    public static final String verNum =  "1.1";
      public static final String modName = "SkidGen";
 
      public static String curseUrl = "https://github.com/WyattTheSkid/SkidGen";
