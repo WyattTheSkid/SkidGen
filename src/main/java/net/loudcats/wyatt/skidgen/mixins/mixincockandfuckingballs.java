393c5630f8e5438d5557b6039b329017ad409346
Wyatt <wyatth120@gmail.com>
2024-11-10 23:35:02 -0500

    i hate java
    fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you fuck you

---
+++ src/main/java/net/loudcats/wyatt/skidgen/mixins/mixincockandfuckingballs.java
@@ -0,0 +1,11 @@
+package net.loudcats.wyatt.skidgen.mixins;
+
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.entity.player.EntityPlayer;
+
+@Mixin(EntityPlayer.class)
+public abstract class mixincockandfuckingballs {
+//do nothing because I just needed to put something here because gradle thinks empty folders dont exist and shoulkd fucking kill itself eroighoerghnoeri
+
+}
