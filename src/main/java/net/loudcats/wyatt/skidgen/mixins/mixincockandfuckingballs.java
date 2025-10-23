5397c446cc9d5255294a5e1ca17ec590b96f4326
Robert <11903511+Roadhog360@users.noreply.github.com>
2025-10-23 02:31:41 +0000

    Update mixincockandfuckingballs.java
    GitHub is the one that ignores empty folders, not Gradle

--- src/main/java/net/loudcats/wyatt/skidgen/mixins/mixincockandfuckingballs.java
+++ src/main/java/net/loudcats/wyatt/skidgen/mixins/mixincockandfuckingballs.java
@@ -6,6 +6,6 @@ import net.minecraft.entity.player.EntityPlayer;
 
 @Mixin(EntityPlayer.class)
 public abstract class mixincockandfuckingballs {
-//do nothing because I just needed to put something here because gradle thinks empty folders dont exist and shoulkd fucking kill itself eroighoerghnoeri
+//do nothing because I just needed to put something here because GitHub thinks empty folders dont exist and shoulkd fucking kill itself eroighoerghnoeri
 
 }
